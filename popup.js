4599bcb994f786bb33ab4777c567fcc860be4cc7
unknown <datdonguyen99@gmail.com>
2021-09-21 21:28:20 +0700

    ver-0

--- popup.js
+++ popup.js
@@ -4,33 +4,37 @@ var a = new Array(),
 a[1] = "B";
 a[2] = "é";
 a[3] = " ";
-a[4] = "m";
-a[5] = "u";
-a[6] = "ố";
-a[7] = "n";
+a[4] = "h";
+a[5] = "ỏ";
+a[6] = "n";
+a[7] = "g";
 a[8] = " ";
-a[9] = "y";
-a[10] = "ê";
-a[11] = "u";
-a[12] = " ";
-a[13] = "n";
-a[14] = "h";
-a[15] = "i";
-a[16] = "ề";
-a[17] = "u";
-a[18] = "♥";
-a[19] = "♥";
-a[20] = "♥";
-a[21] = "♥";
-a[22] = "♥";
-a[23] = "♥";
+a[9] = "c";
+a[10] = "ó";
+a[11] = " ";
+a[12] = "n";
+a[13] = "h";
+a[14] = "ư";
+a[15] = " ";
+a[16] = "a";
+a[17] = "n";
+a[18] = "h";
+a[19] = " ";
+a[20] = "n";
+a[21] = "g";
+a[22] = "h";
+a[23] = "ĩ";
+a[24] = " ";
+a[25] = "đ";
+a[26] = "â";
+a[27] = "u";
 function one() {
   t = document.f.txt.value;
   j = t.length;
   if (j > 0) {
     for (var i = 1; i <= j; i++) {
       n = n + a[i];
-      if (i == 24) {
+      if (i == 28) {
         document.f.txt.value = "";
         n = "";
       }
@@ -48,30 +52,30 @@ function on() {
 function Yeu() {
   $("#divResult").fadeOut(0);
   $("#divResult").html(
-    "</br><h2>Haha Bé đừng có chối nữa nhé!<img src='img/3.gif'</img></h2>"
+    "</br><h2>Thật ra thì anh nói năng khó nghe quá<img src='img/2.gif'</img></h2>"
   );
-  $("#divResult").fadeIn(2000, function () {
+  $("#divResult").fadeIn(3000, function () {
     $("#divResult2").fadeOut(0);
     $("#divResult2").html(
-      "<h2>Anh biết hết ròi nhaaa :v<img src='img/1.gif'/></h2></br>"
+      "<h2>Anh muốn xin lỗi bé :v<img src='img/2 (1).gif'/></h2></br>"
     );
 
-    $("#divResult2").fadeIn(3000, function () {
+    $("#divResult2").fadeIn(4000, function () {
       $("#divResult3").fadeOut(0);
       $("#divResult3").html(
-        "<h2>Thật ra thì anh rất muốn... <img src='img/2.gif'/></h2></br>"
+        "<h2>Nhưng có lẽ lời nói của anh lạnh nhạt quá<img src='img/2.gif'/></h2></br>"
       );
 
       $("#divResult3").fadeIn(5000, function () {
         $("#divResult4").fadeOut(0);
         $("#divResult4").html(
-          "<h2>Đào sâu vào trái tim của bé<img src='img/2 (1).gif'/></h2></br>"
+          "<h2>Nên anh sẽ hành động luôn cho nóng<img src='img/1.gif'/></h2></br>"
         );
 
-        $("#divResult4").fadeIn(3000, function () {
+        $("#divResult4").fadeIn(5000, function () {
           $("#divResult5").fadeOut(0);
           $("#divResult5").html(
-            "<h2>Để 2 chúng ta có thể kết nối lại gần với nhau hơn.<img src='img/4.gif'/></h2></br>"
+            "<h2>Bé dừng giận anh nữa nha!, Anh biết lỗi của mình ròi<img src='img/4.gif'/></h2></br>"
           );
           $("#divResult5").fadeIn(2000);
         });
